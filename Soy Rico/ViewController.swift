96e92393030bf453ce3326a24556a5fb4b80c52a
Porfirio Chávez <hola@porfi.dev>
2022-06-09 10:56:17 -0500

    Initial Commit

---
+++ Soy Rico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Soy Rico
+//
+//  Created by Porfirio Chávez González on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
